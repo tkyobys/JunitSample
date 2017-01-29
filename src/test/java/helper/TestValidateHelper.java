c3c7e148841123e7ed0de564016e335ecb96e639
ohbayashi.takuya <ohbayashi.takuya@gmail.com>
2017-01-29 22:04:42 +0900

    検証に利用するメソッドを修正した。

--- src/test/java/helper/TestValidateHelper.java
+++ src/test/java/helper/TestValidateHelper.java
@@ -1,6 +1,5 @@
 package helper;
 
-import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -10,25 +9,19 @@ public class TestValidateHelper {
 	@Test
 	public void isEmpty_文字列_値あり() {
 		String value = "a";
-		boolean expected = false;
-		boolean actual = ValidateHelper.isEmpty(value);
-		assertThat(actual, is(expected));
+		assertFalse(ValidateHelper.isEmpty(value));
 	}
 
 	@Test
 	public void isEmpty_文字列_空白文字() {
 		String value = "";
-		boolean expected = true;
-		boolean actual = ValidateHelper.isEmpty(value);
-		assertThat(actual, is(expected));
+		assertTrue(ValidateHelper.isEmpty(value));
 	}
 
 	@Test
 	public void isEmpty_文字列_null() {
 		String value = null;
-		boolean expected = true;
-		boolean actual = ValidateHelper.isEmpty(value);
-		assertThat(actual, is(expected));
+		assertTrue(ValidateHelper.isEmpty(value));
 	}
 
 }
